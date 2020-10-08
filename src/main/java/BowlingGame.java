af8157140a5eae02b8854b3fbeb9952487654221
KyleXu <kyle_x@foxmail.com>
2020-10-08 12:07:12 +0800

    [XTH] have two bonus throw if beat all

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,14 +1,33 @@
 public class BowlingGame {
-  private int currentRoundScore = 0;
+  // private int currentRoundScore = 0;
+  private int [][] throwScore = new int[10][2];
+  private int [] roundScore = new int[10];
 
-  public void throwBall(int fallNumber) throws Exception {
-    if (currentRoundScore == 10){
+
+  public void throwBall(int roundNum, int throwNum,int fallNumber) throws Exception {
+    if (throwScore[roundNum][0] == 10 && throwNum == 1){
       throw new RuntimeException("no chance to throw");
     }
-    currentRoundScore += fallNumber;
+    throwScore[roundNum][throwNum] += fallNumber;
   }
 
-  public int getCurrentRoundScore() {
-    return currentRoundScore;
+  public void calculateRoundScore() {
+    for (int i = 0; i < 10; i++){
+      roundScore[i] = throwScore[i][0] + throwScore[i][1];
+    }
+
+    for (int i = 0; i < 9; i++){
+      if (throwScore[i][0] == 10){
+        roundScore[i] += throwScore[i+1][0] + throwScore[i+1][1];
+      }
+    }
+  }
+
+  public int[][] getThrowScore(){
+    return throwScore;
+  }
+
+  public int[] getRoundScore(){
+    return roundScore;
   }
 }
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -18,19 +18,30 @@ public class BowlingGameTest {
     public void throw_twice_less_than_ten_Score_is_sum() throws Exception {
       int firstThrow = 4;
       int secondThrow = 5;
-      bowlingGame.throwBall(firstThrow);
-      bowlingGame.throwBall(secondThrow);
-      assertEquals(9, bowlingGame.getCurrentRoundScore());
+      bowlingGame.throwBall(0,0,firstThrow);
+      bowlingGame.throwBall(0,1,secondThrow);
+      bowlingGame.calculateRoundScore();
+      assertEquals(9, bowlingGame.getRoundScore()[0]);
     }
 
     @Test
     public void cant_throw_if_first_throw_beat_all() throws Exception {
       int beatAll = 10;
-      this.bowlingGame.throwBall(beatAll);
+      this.bowlingGame.throwBall(0,0,beatAll);
       try {
-        bowlingGame.throwBall(2);
+        bowlingGame.throwBall(0,1,2);
       }catch (RuntimeException runtimeException){
         assertThat(runtimeException.getMessage(), is("no chance to throw"));
       }
+    }
+
+    @Test
+    public void first_throw_beatALL_have_other_two_throws() throws Exception {
+        bowlingGame.throwBall(0,0,10);
+        bowlingGame.throwBall(1,0,3);
+        bowlingGame.throwBall(1,1,2);
+        bowlingGame.calculateRoundScore();
+        assertEquals(15, bowlingGame.getRoundScore()[0]);
+        assertEquals(5, bowlingGame.getRoundScore()[1]);
     }
 }
